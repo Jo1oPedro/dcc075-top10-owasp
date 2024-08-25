f3f95e1a9c15da4c43d6b24c5f9653e003b2078b
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-08-25 14:16:35 -0300

    Add top 3 owasp problem injection.

--- src/Controllers/BankController.php
+++ src/Controllers/BankController.php
@@ -6,20 +6,19 @@ class BankController extends Controller
 {
     public function __invoke(string $bankAccountId): void
     {
-        /*$statement = $this->connection->prepare("SELECT * FROM bankAccounts WHERE id = {$bankAccountId}");
-        $statement->execute();
-        $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
-        dd($results);*/
+        /**
+         * To make a depency injection here, the user only need to pass OR 1=1 in the url
+         * like: http://localhost:8080/bankAccount?id=5 OR 1=1
+         */
         $statement = $this->connection->prepare("
                 SELECT t1.*, t2.* 
                 FROM users as t1 JOIN 
-                    (SELECT * FROM bankAccounts WHERE id = ?) as t2
+                    (SELECT * FROM bankAccounts WHERE id = $bankAccountId) as t2
                 ON t1.id = t2.user_id
             "
         );
-        $statement->execute([$bankAccountId]);
+        $statement->execute();
         $results = $statement->fetch(\PDO::FETCH_ASSOC);
-
         view("bankAccount", [
             "results" => $results
         ]);
