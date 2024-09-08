d12ba9c67768e5282f99d84eaf32c26a8a97fdc5
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-09-08 11:22:57 -0300

    add recover password question to the user register

--- src/Controllers/RegisterController.php
+++ src/Controllers/RegisterController.php
@@ -10,8 +10,8 @@ class RegisterController extends Controller
         $stmt = $this->connection->prepare("SELECT * FROM users WHERE email = ?");
         $stmt->execute([$data["email"]]);
         if(!$stmt->rowCount()) {
-            $stmt = $this->connection->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
-            $stmt->execute([$data["email"], $data["password"]]);
+            $stmt = $this->connection->prepare("INSERT INTO users (email, password, recover_password) VALUES (?, ?, ?)");
+            $stmt->execute([$data["email"], $data["password"], $data["recover_password"]]);
             $stmt = $this->connection->prepare("SELECT * FROM users WHERE email = ?");
             $stmt->execute([$data["email"]]);
             $user = $stmt->fetch(\PDO::FETCH_ASSOC);
